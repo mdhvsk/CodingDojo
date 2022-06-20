e25c59505371f361351ef55a8caefa479f2bc9c8
madhavasok <madhavasok@gmail.com>
2022-06-20 13:33:55 -0700

    HTML work 6/20/22

---
+++ Pre_Coding_Bootcamp/Asok-Madhav_JS_tinkering.js
@@ -0,0 +1,11 @@
+childHeight = 65;
+function isChildTallEnoughToRide () {
+    if (childHeight > 55) {
+        console.log("Get on that ride, kiddo!");
+    }
+    else {
+        console.log("Sorry kiddo. Maybe next year!")
+    }
+}
+
+isChildTallEnoughToRide();
