4dc4c090d72b30a080c3c10ae9c541514e57274d
madhavasok <madhavasok@gmail.com>
2022-06-22 10:14:14 -0700

    week1Wed Updates

---
+++ WebFundamentals/algorithms/week1day2.js
@@ -0,0 +1,23 @@
+// Data types
+/*
+List of JS data types
+boolean
+null
+undefined
+strings
+numbers (int float)
+
+
+
+
+
+
+
+*/
+
+
+var inNOutP = 3.99
+var quantity = 4
+
+
+console.log(inNOutP*quantity);
