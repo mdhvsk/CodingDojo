8c7456654caf328947ce2099972d37e9101d0bfe
Madhav Asok <95248875+mdhvsk@users.noreply.github.com>
2022-07-27 16:18:24 -0700

    Delete python/google_api directory

--- python/google_api/demo.py
+++
@@ -1,9 +0,0 @@
-from pprint import pprint
-from Google import Create_Service
-
-CLIENT_SECRET_FILE = 'client_secret_GoogleCloudDemo.json'
-API_NAME = 'calendar'
-API_VERSION = 'v3'
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
